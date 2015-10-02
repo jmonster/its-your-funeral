61a2aba25e87027de3a03655dbc6be1baaa5df08
Johnny Domino <jvdomino@gmail.com>
2015-10-02 14:25:14 -0700

    redirect authenticated users away from /login

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,5 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  
+
+  beforeModel: function(transition) {
+    const isAuthenticated = this.get('session.isAuthenticated');
+
+    if (isAuthenticated) {
+      // ensure Dropbox is linked
+      const dropboxToken = this.get('session.currentUser.dropboxToken');
+      if (!dropboxToken) {
+        return this.transitionTo('dropbox');
+      } else {
+        return this.transitionTo('slide-show');
+      }
+    }
+  }
 });
